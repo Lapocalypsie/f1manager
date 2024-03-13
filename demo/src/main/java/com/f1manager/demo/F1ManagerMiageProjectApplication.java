8e15297851c6438f36867955f42518a65389e29a
Lapocalypsie <jrasolosoa@gmail.com>
2024-03-13 16:23:56 +0100

    first commit

---
+++ demo/src/main/java/com/f1manager/demo/F1ManagerMiageProjectApplication.java
@@ -0,0 +1,13 @@
+package com.f1manager.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ManagerMiageProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1ManagerMiageProjectApplication.class, args);
+	}
+
+}
