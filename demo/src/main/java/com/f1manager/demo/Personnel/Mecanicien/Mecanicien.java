677cc355f71cc3caef921c86036958bdd063034b
lapocalypsie <jrasolosoa@gmail.com>
2024-04-23 14:51:30 +0200

    Refactor Personne Packages

---
+++ demo/src/main/java/com/f1manager/demo/Personnel/Mecanicien/Mecanicien.java
@@ -0,0 +1,8 @@
+package com.f1manager.demo.Personnel.Mecanicien;
+
+import com.f1manager.demo.Personnel.Personne;
+
+public class Mecanicien extends Personne {
+    private double vitesse;
+    private double performance;
+}
